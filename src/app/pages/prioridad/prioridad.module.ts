b26387fc0fbda00a5144473987504646f7a28b8a
Emmanuel Lara <ea.lara21@info.uas.edu.mx>
2023-09-23 13:04:08 -0700

    Avance

--- src/app/pages/prioridad/prioridad.module.ts
+++ src/app/pages/prioridad/prioridad.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { PrioridadComponent } from './prioridad.component';
 import { routing } from './prioridad.routing';
+import { NbInputModule } from '@nebular/theme';
+import { NbButtonModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    routing
+    routing,
+    NbInputModule,
+    NbButtonModule
   ],
   declarations: [
     PrioridadComponent
